7abc96c3a836186f01cc15126ea9fbf6e147ae09
luna-data <hjl7891@gmail.com>
2025-10-02 22:07:40 +0900

    +

--- chapter_08/9.25/label3.py
+++ chapter_08/9.25/label3.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 root = Tk()
-photo=PhotoImage(file="dog2.gif")
+photo=PhotoImage(file="chapter_08/9.25/dog2.gif")
 label=Label(root,image=photo)
 label.pack()
 root.mainloop()
