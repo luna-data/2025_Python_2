25e556fb1438068742d3a77f1ac867d10a3556c3
luna-data <hjl7891@gmail.com>
2025-11-24 23:22:01 +0900

    2

--- 과제/file_tkinter_assignment/fitk_01.py
+++ 과제/file_tkinter_assignment/fitk_01.py
@@ -1,18 +1,13 @@
 import os
 base_dir=os.path.dirname(__file__)
-filename=input("텍스트 파일 이름을 입력하세요: ").strip()
-filepath=os.path.join(base_dir, filename)
-infile=open(filepath, "r",encoding="utf-8")
+
+filename=os.path.join(base_dir, input("텍스트 파일 이름을 입력하세요: "))
 
 try:
-    with open("infile",'r') as file:
+    with open(filename,'r',encoding="utf-8") as file:
         content=file.read()
-    print(content)
+        print(content)
+        file.close()
 
 except FileNotFoundError:
     print("파일을 찾을 수 없습니다!")
-
-finally:
-    print("파일 처리 완료.")
-
-
