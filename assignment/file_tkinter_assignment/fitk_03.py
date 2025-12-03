bd359ce98df0c89804f57fa4818e01d1687764b9
luna-data <hjl7891@gmail.com>
2025-12-03 21:39:04 +0900

    11

--- assignment/file_tkinter_assignment/fitk_03.py
+++ assignment/file_tkinter_assignment/fitk_03.py
@@ -1,10 +1,10 @@
-infile1=open("assignment/file_tkinter_assignment/file1.txt","r")
-infile2=open("assignment/file_tkinter_assignment/file2.txt","r")
+infile1=open("assignment/file_tkinter_assignment/file1.txt","r",encoding="utf-8")
+infile2=open("assignment/file_tkinter_assignment/file2.txt","r",encoding="utf-8")
 
 line1=infile1.readline()
 line2=infile2.readline()
 
-outfile=open("assignment/file_tkinter_assignment/output.txt","w")
+outfile=open("assignment/file_tkinter_assignment/output.txt","w",encoding="utf-8")
 
 for i in (line1,line2):
     i=i.rstrip()
